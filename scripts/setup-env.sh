9e8e8f40c609d4f3d00d49591c90dc897503e839
ewood <ericwood73+github@gmail.com>
2025-03-31 09:23:16 -0400

    Fix exporting of env.

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Export all variables from .env
-while read -r line; do export "$line"; done < ~/.env
+# Export all non-comment, non-blank variables from .env
+while read -r line; do
+    # Skip empty lines and comments
+    [[ -z "${line}" || "${line}" =~ ^[[:space:]]*# ]] || export "$line"
+done < ~/.env
